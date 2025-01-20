5a7ecdde9e6d678a82256aa453cb8e9a17a1d178
Manan1903 <goyalmanan786@gmail.com>
2025-01-20 14:36:18 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    unsigned int a =5,b= 9;
+    
+    printf("a=%u,b=%u\n",a,b);
+    printf("a&b=%u\n",a&b);
+    printf("a|b=%u\n",a|b);
+    printf("a^b=%u\n",a^b);
+    printf("~a=%u\n",~a);
+    printf("b<<1=%u\n",b<<1);
+    printf("b>>1=%u\n",b>>1);
+
+    return 0;
+}
